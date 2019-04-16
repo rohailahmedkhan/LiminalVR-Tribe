44686bd17a9ca138e1fa2bff65b5951b7e28fe65
Vishwas G Kini <vgkini38@gmail.com>
2019-04-17 01:25:52 +1000

    Add files via upload
    Basic Prototype CS code

---
+++ Back End/scrip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrip : MonoBehaviour
+{
+    public float speed = 0.3f;
+    public GameObject a=null;
+    public GameObject b=null;
+    public GameObject c=null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < 3.0f)            
+        transform.position += Vector3.forward * Time.deltaTime * speed;
+        if (transform.position.z > -2.8f && transform.position.z < 0.0f)
+            a.SetActive(true);
+        if (transform.position.z > 0.0f && transform.position.z < 1.0f)
+            c.SetActive(true);
+        if (transform.position.z > 1.7f)
+            b.SetActive(true);
+
+
+
+    }
+}
