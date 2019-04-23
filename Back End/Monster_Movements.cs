be4425a6f604a5044b677096a3f4ce212972e816
Shrutik Panchal <sypanchal@deakin.edu.au>
2019-04-23 23:19:44 +1000

    New: Movement of Monsters according to Player's Current position.

---
+++ Back End/Monster_Movements.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    GameObject target;
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.Find("Player");
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+    }
+}
---
+++ Back End/Player_Movements.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0.9f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * Time.deltaTime * speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name != "ground")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
